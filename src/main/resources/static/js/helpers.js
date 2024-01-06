86cafc1b3008c58ad8ffc34e0d5b60fd24679393
Xristen <lim.christen@gmail.com>
2024-01-06 16:13:28 +0800

    Display error message when products not fetched, and other slight tweaks.

--- src/main/resources/static/js/helpers.js
+++ src/main/resources/static/js/helpers.js
@@ -1,34 +1,38 @@
 var data = [];
 // Create instance of productList, i.e. controller class
-const productController = new ProductController()
+const productController = new ProductController();
 
 const url = document.location.search;
 let urlParams = new URLSearchParams(url);
 let type = urlParams.get("type");
-// console.log("Type is:" + type); // For debugging 
+// console.log("Type is:" + type); // For debugging
 
 // Fetch data from API
-async function fetchData(type="") {
-    if (type === null) {
-        try {
-            let response = await fetch("/api/products");
-            let data = await response.json();
-            productController.displayList(data);
-        }
-        catch (error) {
-            console.error("Error fetching products from API: ", error);
-        }
+async function fetchData(type = "") {
+  if (type === null) {
+    try {
+      let response = await fetch("/api/products");
+      let data = await response.json();
+      productController.displayList(data);
+    } catch (error) {
+      loadError(error);
     }
-    else {
-        try {
-            let response = await fetch(`/api/products?type=${type}`);
-            let data = await response.json();
-            productController.displayList(data);
-        }
-        catch (error) {
-            console.error("Error fetching products from API: ", error);
-        }
+  } else {
+    try {
+      let response = await fetch(`/api/products?type=${type}`);
+      let data = await response.json();
+      this.displayList(data);
+    } catch (error) {
+      loadError(error);
     }
+  }
+}
+function loadError(error) {
+  const unorderedList = document.getElementById("showList");
+  unorderedList.classList.add("alert", "alert-danger", "w-50", "translate-middle-x", "start-50", "mt-3");
+                      unorderedList.setAttribute("role", "alert");
+                      unorderedList.innerHTML = `<span>${error}.</span>`;
+                      throw new Error("Fetching of data failed.");
 }
 // Initial fetch - Load immediately when the script is loaded to fetch initial data.
 fetchData(type);
