8836581127368e1fb8b37119e0186c577da48f7e
Xristen <lim.christen@gmail.com>
2024-01-15 15:51:26 +0800

    rectified typo

--- src/main/resources/static/js/product-controller.js
+++ src/main/resources/static/js/product-controller.js
@@ -82,7 +82,7 @@ class ProductController {
                     // Append div to create some space before the next header. Not applicable to the first header
                     if (currentType != "") {
                         let spaceBeforeNextHeader = document.createElement("div")
-                        spaceBeforeNextHeader.className="git typeHeader";
+                        spaceBeforeNextHeader.className="typeHeader";
                         unorderedList.appendChild(spaceBeforeNextHeader);
                     }
                     currentType = dataSet[i].type.toUpperCase();
